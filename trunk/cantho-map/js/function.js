1f8e9e6149bec57d9663707312cee9f2037af7c7
tvhoang75 <tvhoang75@b3d5cadd-b24d-ad91-918c-aba5ebc324a3>
2011-02-11 10:26:22 +0000

    add libs/jquery/contextmenu add js/function.js modified map.php,marker2.php,rightclick.php

--- trunk/cantho-map/js/function.js
+++ trunk/cantho-map/js/function.js
@@ -1,1 +1,20 @@
 // JavaScript Document
+/*
+	@tao maker tren mot lop
+	@params:
+		map  : Map
+		layer: Layer
+		pixel: Pixel
+	@return: void
+*/
+	function createMarker(map, layer, pixel) {
+		var feature;
+		var lonlat = map.getLonLatFromViewPortPx(pixel);
+		feature = (new OpenLayers.Feature.Vector(new OpenLayers.Geometry.Point(
+				lonlat.lon, lonlat.lat)));
+		layer.addFeatures(feature);		
+		//dang ky su kien mousedown cho feature		
+		feature.events.register('mousedown', feature, function(e) {
+			alert("Ban vua click len mot marker");
+		});
+	}
