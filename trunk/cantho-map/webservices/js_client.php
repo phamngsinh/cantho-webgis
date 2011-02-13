8829af7c563340d5f4cfac136d482f7a318f92bb
tvhoang75 <tvhoang75@b3d5cadd-b24d-ad91-918c-aba5ebc324a3>
2011-02-13 12:43:27 +0000

    add folder webservices

---
+++ trunk/cantho-map/webservices/js_client.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+<script language="javascript" src="../libs/jquery/jquery-1.4.4.js"></script>
+<script language="javascript" defer="defer">
+$(document).ready(function(){
+						   $("#btn_reverse").click(function(){
+															 callWebservice($("#txt_str").val());
+															 });
+						   });//end function $(document).ready(function()
+
+
+//var url = 'http://localhost/cantho-map/webservice/server.php?wsdl'; 
+ var url = 'http://localhost/cantho-map/webservice/service.php?wsdl';
+function callWebservice(str) /*Add parameters and what not*/ { 
+ /*
+    //Example of a xml request probably put this is the parameter 
+	var soapMessage = "<\?xml version='1.0' encoding='utf-8'\?>";
+	soapMessage += "<soapenv:Envelope xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' xmlns:xsd='http:";
+	soapMessage += "//www.w3.org/2001/XMLSchema' xmlns:soapenv='http://schemas.xmlsoap.org/soap/envelope/' xmlns:urn='urn:server'>";
+   soapMessage += " <soapenv:Header/>";
+   soapMessage += " <soapenv:Body>";
+   soapMessage += "  <urn:pollServer soapenv:encodingStyle='http://schemas.xmlsoap.org/soap/encoding/'>";
+   soapMessage += "      <value xsi:type='xsd:string'>Good</value>";
+   soapMessage += "   </urn:pollServer>";
+   soapMessage += " </soapenv:Body>";
+   soapMessage += " </soapenv:Envelope>";
+   */
+   var soapMessage = "<\?xml version='1.0' encoding='utf-8'\?>";
+	   soapMessage += "<soapenv:Envelope xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance'      	   xmlns:xsd='http://www.w3.org/2001/XMLSchema' xmlns:soapenv='http://schemas.xmlsoap.org/soap/envelope/'>";
+       soapMessage += "<soapenv:Header/>";
+       soapMessage += " <soapenv:Body>";
+       soapMessage += "<Reverse soapenv:encodingStyle='http://schemas.xmlsoap.org/soap/encoding/'>";
+       soapMessage += "  <text xsi:type='xsd:string'>"+str+"</text>";
+       soapMessage += "</Reverse>";
+       soapMessage += "</soapenv:Body>";
+	   soapMessage += "</soapenv:Envelope>";
+	//alert('request: '+soapMessage); 
+    $.ajax({ 
+        type: 'POST', 
+        url: url, 
+        cache: false, 
+        success: function(data){ 
+            //var xml = data.xml; 
+			var xml=$(data);
+			//xml.find("return").text();
+            //[do something with the xml] 
+			//alert('ket qua: '+xml.find("result").text());
+			$("#lbl_result").html("Ket qua: "+xml.find("result").text());
+        }, 
+        error: function(data){ 
+            var xml = $(data); 
+            //[do something with the xml] 
+			//alert('Loi : '+xml.find("faultstring").text());
+			$("#lbl_result").html('Ket qua : ');
+        }, 
+		dataType: 'xml',// kieu du lieu tra ve (response)
+        contentType: 'text/xml', //kieu du lieu gui di (request)
+        data: soapMessage 
+    }); 
+} 
+</script>
+<body>
+<input id="btn_reverse" type="button" value="Dao chuoi" />
+<input id="txt_str" type="text"/>
+<label id="lbl_result">Ket qua: </label>
+</body>
+</html>
---
+++ trunk/cantho-map/webservices/pgrouting.php
@@ -0,0 +1,94 @@
+<?php
+// Database connection settings
+	define("PG_DB" , "postgis");
+	define("PG_HOST", "localhost");
+	define("PG_PORT", "5432");
+	define("PG_USER", "postgres");	
+	define("PG_PASSWORD","admin");
+	define("TABLE", "giaothong");
+	
+	// Retrieve start point
+	//$start = split(' ',$_REQUEST['startpoint']);
+	$start=split(' ','585963.32538 1111336.23539');
+	$startPoint = array($start[0], $start[1]);
+	
+	// Retrieve end point
+	//$end = split(' ',$_REQUEST['finalpoint']);
+	$end = split(' ','585564.55712 1110794.75344');
+	$endPoint = array($end[0], $end[1]);
+	
+	// Find the nearest edge
+	$startEdge = findNearestEdge($startPoint);
+	$endEdge = findNearestEdge($endPoint);
+	// FUNCTION findNearestEdge
+	function findNearestEdge($lonlat) 
+	{
+		// Connect to database
+		$con = pg_connect("dbname=".PG_DB." host=".PG_HOST." port=".PG_PORT." user=".PG_USER." password=".PG_PASSWORD);
+		$sql = "SELECT gid, source, target, the_geom,
+		distance(the_geom, GeometryFromText(
+		'POINT(".$lonlat[0]." ".$lonlat[1].")', 4326)) AS dist
+		FROM ".TABLE."
+		WHERE the_geom && setsrid(
+		'BOX3D(".($lonlat[0]-0.1)."
+		".($lonlat[1]-0.1).",
+		".($lonlat[0]+0.1)."
+		".($lonlat[1]+0.1).")'::box3d, 4326)
+		ORDER BY dist LIMIT 1";
+		$query = pg_query($con,$sql);
+		$edge['gid'] = pg_fetch_result($query, 0, 0);
+		$edge['source'] = pg_fetch_result($query, 0, 1);
+		$edge['target'] = pg_fetch_result($query, 0, 2);
+		$edge['the_geom'] = pg_fetch_result($query, 0, 3);
+		// Close database connection
+		pg_close($con);
+		return $edge;
+	}
+	//print_r($startEdge);
+	
+	$sql = "SELECT rt.gid, ST_AsGeoJSON(rt.the_geom) AS geojson,
+		length(rt.the_geom) AS length, ".TABLE.".gid
+		FROM ".TABLE.",
+		(SELECT gid, the_geom
+		FROM dijkstra_sp_delta(
+		'".TABLE."',
+		".$startEdge['source'].",
+		".$endEdge['target'].",0.1)
+		) as rt
+		WHERE ".TABLE.".gid=rt.gid;";
+		//echo $sql;
+		// Connect to database
+		$dbcon = pg_connect("dbname=".PG_DB." host=".PG_HOST." port=".PG_PORT." user=".PG_USER." password=".PG_PASSWORD);
+		// Perform database query
+		$query = pg_query($dbcon,$sql);
+		//echo pg_num_rows($query);
+		
+	
+		// Return route as GeoJSON
+		$geojson = array(
+			'type' => 'FeatureCollection',
+			'features' => array()
+			);
+			// Add edges to GeoJSON array
+			while($edge=pg_fetch_assoc($query)) {
+			$feature = array(
+			'type' => 'Feature',
+			'geometry' => json_decode($edge['geojson'], true),
+			'crs' => array(
+			'type' => 'EPSG',
+			'properties' => array('code' => '4326')
+		),
+		'properties' => array(
+		'id' => $edge['id'],
+		'length' => $edge['length']
+		)
+		);
+		// Add feature array to feature collection array
+		array_push($geojson['features'], $feature);
+		}
+		// Close database connection
+		pg_close($dbcon);
+		// Return routing result
+		header('Content-type: application/json',true);
+		echo json_encode($geojson);				
+?>
