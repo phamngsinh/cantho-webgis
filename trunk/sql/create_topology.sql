39c8554c9583bc51d275bd0c8fe2254773c8aa98
tvhoang75 <tvhoang75@b3d5cadd-b24d-ad91-918c-aba5ebc324a3>
2011-02-25 15:53:51 +0000

    modified create_topology: da chinh sua phu hop voi thuat toan hien tai

--- trunk/sql/create_topology.sql
+++ trunk/sql/create_topology.sql
@@ -3,21 +3,22 @@
 ALTER TABLE giaothong ADD COLUMN source integer;
 ALTER TABLE giaothong ADD COLUMN target integer;
 ALTER TABLE giaothong ADD COLUMN length double precision;
-ALTER TABLE giaothong ADD COLUMN cost double precision;
-ALTER TABLE giaothong ADD COLUMN rcost double precision;
+--ALTER TABLE giaothong ADD COLUMN cost double precision;
+--ALTER TABLE giaothong ADD COLUMN rcost double precision;
 
 SELECT assign_vertex_id('giaothong', 0.000001, 'the_geom', 'gid');
 
 UPDATE giaothong SET length=length(the_geom);
+/*
 UPDATE giaothong SET cost=length(the_geom);
 UPDATE giaothong SET rcost=length(the_geom);
 UPDATE giaothong SET rcost=length(the_geom)+1000000
 WHERE mot_chieu=1;
-
+*/
 CREATE INDEX source_idx ON giaothong(source);
 CREATE INDEX target_idx ON giaothong(target);
 CREATE INDEX geom_idx ON giaothong USING GIST(the_geom GIST_GEOMETRY_OPS);
-
+/*
 ALTER TABLE giaothong ADD COLUMN x1 double precision;
 ALTER TABLE giaothong ADD COLUMN y1 double precision;
 ALTER TABLE giaothong ADD COLUMN x2 double precision;
@@ -26,5 +27,5 @@ UPDATE giaothong SET x1 = x(ST_startpoint(the_geom));
 UPDATE giaothong SET y1 = y(ST_startpoint(the_geom));
 UPDATE giaothong SET x2 = x(ST_endpoint(the_geom));
 UPDATE giaothong SET y2 = y(ST_endpoint(the_geom));
-
+*/
 
